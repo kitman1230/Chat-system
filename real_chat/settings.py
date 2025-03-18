fca63a79cd5ef938edd58e4e4a130e2e4638a448
kitman1230 <kitmanwai1230@gmail.com>
2025-03-18 13:42:41 +0800

    add asynchronous chat function

--- real_chat/settings.py
+++ real_chat/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1", "*"]
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -77,8 +78,12 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "real_chat.wsgi.application"
+# WSGI_APPLICATION = "real_chat.wsgi.application"
+ASGI_APPLICATION = "real_chat.asgi.application"
 
+CHANNEL_LAYERS = {
+    "default": {"BACKEND": "channels.layers.InMemoryChannelLayer"},
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
